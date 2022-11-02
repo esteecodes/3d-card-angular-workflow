86b6f52ac1e4dd8102a8f2a22f033a56352c881b
Esther White <esteewhite@hotmail.com>
2022-11-02 23:01:40 +0200

    mat icon custom svg

--- 3d-card-angular/src/app/app.module.ts
+++ 3d-card-angular/src/app/app.module.ts
@@ -1,15 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { ProfileCardComponent } from './components/profile-card/profile-card.com
     ProfileCardComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
@@ -27,4 +30,13 @@ import { ProfileCardComponent } from './components/profile-card/profile-card.com
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor (
+    private domSanitizer: DomSanitizer,
+    private matIconRegistry: MatIconRegistry )
+    { matIconRegistry
+      .addSvgIcon('angular',this.domSanitizer
+      .bypassSecurityTrustResourceUrl('assets/angular-mine.svg')
+    )}
+}
